85af542a5b08048492f5460e3f0f7f5ec7835765
ferflores <ferflores@gmail.com>
2016-11-20 11:10:27 -0600

    base64 not required

--- auth.go
+++ auth.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"net/http"
 	"time"
@@ -12,9 +11,7 @@ import (
 func ValidateToken(res http.ResponseWriter, req AuthorizedRequest) *jwt.Token {
 	defToken := jwt.Token{Valid: false}
 
-	decoded, _ := base64.StdEncoding.DecodeString(req.AuthToken)
-
-	token, err := jwt.ParseWithClaims(string(decoded), &Claims{}, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.ParseWithClaims(string(req.AuthToken), &Claims{}, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected siging method")
 		}
@@ -43,5 +40,5 @@ func GetToken(rw http.ResponseWriter, userName string, isAdmin bool) string {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	signedToken, _ := token.SignedString([]byte(config.AuthKey))
-	return base64.StdEncoding.EncodeToString([]byte(signedToken))
+	return signedToken
 }

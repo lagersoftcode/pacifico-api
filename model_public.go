1b89ca32706461e6e0098b39641636397bce2750
ferflores <ferflores@gmail.com>
2016-11-30 11:54:54 -0600

    stats in public user

--- model_public.go
+++ model_public.go
@@ -1,6 +1,10 @@
 package main
 
 type PublicUser struct {
-	ID       string
-	UserName string
+	ID                  string
+	UserName            string
+	Stats_TotalTrophies uint
+	Stats_TotalMedals   uint
+	Stats_TotalKudos    uint
+	Stats_TotalScore    uint
 }
